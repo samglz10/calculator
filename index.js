b4f4a6e09d53197a9bb9d6001711b0044294f4c4
Samuel <samglz10@gmail.com>
2024-06-12 19:01:43 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const add = function() {
+	
+};
+
+const subtract = function() {
+	
+};
+
+const sum = function() {
+	
+};
+
+const multiply = function() {
+
+};
+
+const power = function() {
+	
+};
+
+const factorial = function() {
+	
+};
