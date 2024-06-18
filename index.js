02a9c41143f523c7bf990eede7d783607f09bfc0
Samuel <samglz10@gmail.com>
2024-06-17 21:44:02 -0400

    recreating variables

--- index.js
+++ index.js
@@ -1,25 +1,84 @@
+/*
+v1
 const calculator = document.getElementById("calculator-container");
 const digits = document.getElementById('digits')
-const operationBtns = document.getElementById('operations')
+const operationBtns = document.getElementsByClassName('.operations')
 const display = document.getElementById('display')
-
+*/
+const calculator = document.getElementById("calculator-container");
+const operations = document.querySelectorAll('#operations');
+const numbers = document.querySelectorAll('.numbers');
+const clearBtn = document.querySelector('#clear');
+const backspaceBtn = document.querySelector('#backspaceBtn');
+const totalButton = document.querySelector('#total');
+const previousInputData = document.querySelector('#previous-input-data');
+const CurrentInputData = document.querySelector('#current-input-data');
+const display = document.querySelector('output')
+/*
 let total = 0;
 let x = 4;
 let y = 4;
 let currentOperation = '';
-let displayArray = [];
+let displayArray = [];*/
+console.log(operations)
+console.log(numbers)
+let previousInputData = '';
+let CurrentInputData ='';
 
-
-const numbers = [1,2,3,4,5,6,7,8,9,0];
-const operations=['Clear','Back', '+','-','/','*','=']
+//const numbers = [1,2,3,4,5,6,7,8,9,0];
+//const operations=['Clear','Back', '+','-','/','*','=']
 
 const operators = {
+    clear: ()=>{},
+    delete: ()=>{},
     '+': (x,y)=>x+y,
     '-': (x, y) => x - y,
     '*': (x, y) => x * y,
     '/': (x, y) => x / y
 }
+//operators
+for(let i = 0; i < operations.length; i++){
 
+    const singleOperation = operations[i]
+    //console.log(singleOperation);
+    singleOperation.addEventListener('click',(e)=>{
+        console.log('clicked', e.target.value)
+        if(e.target.value === undefined){
+            return
+        }
+        
+        if(e.target.value === operations[0]){
+           console.log(e.target.value)
+        }
+        if(e.target.value === operations[1]){
+            console.log(e.target.value)
+        }
+
+    })
+
+}
+//numbers
+for(let i = 0; i < numbers.length; i++){
+    const singleNumber = numbers[i]
+    //console.log(singleNumber);
+    singleNumber.addEventListener('click',(e)=>{
+        console.log('clicked', e.target.value)
+        if(e.target.value === undefined){
+            return
+        }
+        
+        if(e.target.value === numbers[0]){
+           console.log(e.target.value)
+        }
+        if(e.target.value === numbers[1]){
+            console.log(e.target.value)
+        }
+    })
+}
+//clear and back buttons
+
+
+/* v1
 //numbers on screen
 numbers.forEach((number)=>{
     //console.log(number);
@@ -82,7 +141,7 @@ digits.addEventListener('click',(e)=>{
     
 })
 
-console.log(displayArray)
+console.log(displayArray);
 /*
 const add = (x,y)=> {
     console.log(`${x}+${y} = ${x + y}`)
